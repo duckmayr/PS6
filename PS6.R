31b591fea0c3382b78716bd0c8a057ff280bd8ac
JB Duck-Mayr <j.duckmayr@gmail.com>
2017-04-20 12:09:51 -0500

    add tests

--- PS6.R
+++ PS6.R
@@ -1,6 +1,5 @@
-sg.int <- function(g, ..., lower, upper, dimensions){
+sg.int <- function(g, lower, upper, dimensions, parallel=TRUE, ...){
   require("SparseGrid")
-  require('parallel')
   
   lower <- floor(lower)
   upper <- ceiling(upper)
@@ -21,10 +20,68 @@ sg.int <- function(g, ..., lower, upper, dimensions){
     weights <- c(weights, sp.grid$weights)
   }
   
-  clus <- makeCluster(max(detectCores() - 1, 1))
-  gx.sp <- parApply(cl=clus, X=nodes, MARGIN=1, FUN=g, ...)
-  val.sp <- gx.sp %*% weights
-  stopCluster(clus)
+  if ( parallel ) {
+    require('parallel')
+    clus <- makeCluster(max(detectCores() - 1, 1))
+    
+    gx.sp <- parApply(cl=clus, X=nodes, MARGIN=1, FUN=g, ...)
+    val.sp <- gx.sp %*% weights
+    
+    stopCluster(clus)
+  } else {
+    gx.sp <- apply(X=nodes, MARGIN=1, FUN=g, ...)
+    val.sp <- gx.sp %*% weights
+  }
   
   return(val.sp)
 }
+
+## Tests:
+
+library(mvtnorm)
+library(testthat)
+
+myNorm <- function(x, dimensions){
+  dmvnorm(x, mean=rep(0, dimensions), sigma=diag(rep(1, dimensions)))
+}
+
+realAns <- function(lower, upper, dimensions){
+  return(as.numeric(pmvnorm(rep(lower, dimensions), rep(upper, dimensions),
+                            rep(0, dimensions), diag(rep(1, dimensions)))))
+}
+
+comparison <- function(lower, upper, dimensions){
+  return(as.numeric(sg.int(myNorm, rep(lower, dimensions),
+                           rep(upper, dimensions), dimensions,
+                           FALSE, dimensions)))
+}
+
+expect_equivalent(realAns(-2, 2, 2), comparison(-2, 2, 2))
+expect_equivalent(realAns(-2, 2, 4), comparison(-2, 2, 4))
+
+mySine <- function(x){
+  return(sum(sin(x)))
+}
+
+mcInt <- function(a, b, n, dimensions){
+  u <- runif(n*dimensions, a, b)
+  these <- matrix(u, ncol=dimensions)
+  x <- sapply(1:nrow(these), function(x) mySine(c(these[x, ])))
+  return(mean(x)*(b-a)^dimensions)
+}
+
+comparison <- function(lower, upper, dimensions){
+  return(as.numeric(sg.int(mySine, lower=rep(lower, dimensions),
+                           upper=rep(upper, dimensions),
+                           dimensions=dimensions, parallel=FALSE)))
+}
+
+expect_equal(mcInt(0, 9, 100000, 2), comparison(0, 9, 2), tol=0.1)
+expect_equal(mcInt(0, 9, 100000, 3), comparison(0, 9, 3), tol=0.1)
+
+expect_error(sg.int(mySine, lower='rep(0, 3)', upper=rep(9, 3), dimensions=3, parallel=FALSE),
+             'non-numeric argument to mathematical function')
+expect_error(sg.int(mySine, lower=rep(0, 3), upper=rep(9, 3), dimensions='3', parallel=FALSE),
+             "wrong sign in 'by' argument")
+expect_error(sg.int(mySine, lower=rep(9, 3), upper=rep(0, 3), dimensions=3, parallel=FALSE),
+             "lower must be smaller than upper")
